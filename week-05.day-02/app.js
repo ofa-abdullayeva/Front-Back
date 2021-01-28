ce1160d9fe08435003bf70b25227efa907a89ea8
ofa-abdullayeva <abdullayevaofa25@gmail.com>
2021-01-28 11:42:48 +0400

    week-06.day-03

---
+++ week-05.day-02/app.js
@@ -0,0 +1,21 @@
+//Yuxarida verilen array methodlarindan beyendiyiniz 10 eded methodu tetbig ederek kod yazin
+ 1 //concat()
+
+ 2 //pop()
+
+ 3 //push()
+
+ 4 //shift()
+
+ 5 //sort()
+
+ 6 //unshift()
+
+ 7 //reverse()
+
+ 8 //splice()
+
+ 9  //indexOf()
+
+ 10 //slice()
+
